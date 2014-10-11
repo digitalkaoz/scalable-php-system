a597abf227e745c44df27add9a5e19ddebf60633
Robert Schönthal <caziel@gmx.net>
2014-10-11 23:47:42 +0200

    working nginx < php upstream servers

--- containers/nginx/serf-member-join.sh
+++ containers/nginx/serf-member-join.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-if [ "x${SERF_SELF_ROLE}" != "xlb" ]; then
-    echo "Not an lb. Ignoring member join."
+if [ "x${SERF_SELF_ROLE}" != "xweb" ]; then
+    echo "Not an nginx. Ignoring member join."
     exit 0
 fi
 
 while read line; do
     ROLE=`echo $line | awk '{print \$3 }'`
-    SERVER=`echo $line | awk '{printf "server %s:9000;", $2}'`
 
     if [ "x${ROLE}" == "xphp" ]; then
-        sed -i '/PHP_FPM_NODES/a\		'$SERVER'' /etc/nginx/nginx.conf
+        IP=`echo $line | awk '{print \$2 }'`
+        sed -i '/PHP_FPM_NODES/a\		server '$IP':9000' /etc/nginx/nginx.conf
     fi
 done
 
